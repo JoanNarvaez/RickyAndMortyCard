45ec191f48f4bfc87fb5ed995179ff06a518ba9d
JOAN <joanandres10@gmail.com>
2024-07-13 16:38:51 -0500

    Actividad 2 ApiCard RickyAndMorty

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ListaPersonajesComponent } from './components/lista-personajes/lista-personajes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+
+    {
+        path:'',
+        component:ListaPersonajesComponent
+    }
+    
+];
